63d7eb891092766bdff83d0c97a201cbc70dc8cb
werliton <werlitoncarlos@gmail.com>
2018-06-21 13:28:45 -0300

    Alterando porta

--- web.py
+++ web.py
@@ -26,6 +26,6 @@ def filmes():
     return jsonify({ 'filmes': data})
 
 if __name__ == '__main__':
-    app.run()
-   # port = int(os.environ.get('PORT', 5000))
-    #app.run(host='127.0.0.1', port=port)
+    #app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
