c5062be1c5da329d7db4b999ce02a454a5c26989
werliton <werlitoncarlos@gmail.com>
2018-06-21 15:58:11 -0300

    Criando método para cinema especifico

--- rioanil.py
+++ rioanil.py
@@ -7,27 +7,6 @@ app = Flask(__name__)
 
 @app.route('/api/v1/filmes', methods=['GET'])
 def filmes():
-    html_doc = urlopen("http://www.adorocinema.com/filmes/numero-cinemas/").read()
-    soup = BeautifulSoup(html_doc, "html.parser")
-
-    data = []
-    for dataBox in soup.find_all("div", class_="card card-entity card-entity-list cf"):
-        nomeObj = dataBox.find("h2", class_="meta-title").find("a", class_="meta-title-link")
-        imgObj  = dataBox.find(class_="thumbnail")
-        sinopseObj = dataBox.find(class_="synopsis")
-        dataObj = dataBox.find("span", class_="date")
-        data.append({
-            'nome':nomeObj.text.strip(),
-            'poster':imgObj.img['src'].strip(),
-            'sinopse': sinopseObj.text.strip(),
-            'data': dataObj.text.strip()
-        })
-
-    return jsonify({ 'filmes': data})
-
-
-@app.route('/api/v1/cinema/rioanil', methods=['GET'])
-def rioanil():
     html_doc = urlopen("http://www.rioanil.com.br/cinema").read()
     soup = BeautifulSoup(html_doc, "html.parser")
 
@@ -51,8 +30,7 @@ def rioanil():
 
     return jsonify({ 'filmes': data})
 
-
 if __name__ == '__main__':
-    #app.run()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+   # port = int(os.environ.get('PORT', 5000))
+    #app.run(host='127.0.0.1', port=port)
